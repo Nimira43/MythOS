517c391cf1850ec4ac1b1dc14e0a12731a998a5d
Nick Rabone <n.rabone72@aol.com>
2024-04-28 23:51:03 +0100

    Common JS syntax

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('Node Crash Course')
+const { generateRandomNumber, celsiusToFahrenheit } = require('./utils')
+
+console.log(`Random Number: ${generateRandomNumber()}`)
+console.log(`Celsius to Fahrenheit: ${celsiusToFahrenheit(27)}`)
