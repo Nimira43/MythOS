9dd44c74d3b1a940dbff8616190368eaf2cdf40f
Nick Rabone <n.rabone72@aol.com>
2024-04-29 11:56:52 +0100

    Updated

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const { generateRandomNumber, celsiusToFahrenheit } = require('./utils')
+import { getPosts } from './postController.js'
 
-console.log(`Random Number: ${generateRandomNumber()}`)
-console.log(`Celsius to Fahrenheit: ${celsiusToFahrenheit(27)}`)
+console.log(getPosts())
+
+
+// const { generateRandomNumber, celsiusToFahrenheit } = require('./utils')
+// console.log(`Random Number: ${generateRandomNumber()}`)
+// console.log(`Celsius to Fahrenheit: ${celsiusToFahrenheit(27)}`)
+
