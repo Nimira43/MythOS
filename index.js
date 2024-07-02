fa5ac556fcfd52c580795fb5d1b7562a7b6ba213
Nick Rabone <n.rabone72@aol.com>
2024-07-02 05:24:04 +0100

    Rebooted

--- index.js
+++
@@ -1,9 +0,0 @@
-import getPosts, { getPostsLength } from './postController.js'
-
-console.log(getPosts())
-console.log(`Post Length: ${getPostsLength()}`)
-
-// const { generateRandomNumber, celsiusToFahrenheit }= require('./utils')
-
-// console.log(`Random Number: ${generateRandomNumber()}`)
-// console.log(`Celsius to Fahrenheit: ${celsiusToFahrenheit(32)}`)
