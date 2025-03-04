f96426f4bfd8ef0ea90b999822aa3ad1d77b5638
Nimira43 <n.rabone72@aol.com>
2025-03-04 19:32:06 +0000

    setup post response. Brought in Crypto to create a random id for the post request.

--- posts/index.js
+++ posts/index.js
@@ -1,15 +1,17 @@
 const express = require('express')
+const { randomBytes } = require('crypto')
+
 const app = express()
 const PORT = 4000
 
 const posts = {}
 
 app.get('/posts', (req, res) => {
-
+  res.send(posts)
 })
 
 app.post('/posts', (req, res) => {
-
+  const id = randomBytes(4).toString('hex')
 })
 
 app.listen(4000, () => {
