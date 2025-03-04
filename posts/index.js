38dc6750d6569aed9a6ef68d6893f40a2d313994
Nimira43 <n.rabone72@aol.com>
2025-03-04 19:34:55 +0000

    Completed the post request and response.

--- posts/index.js
+++ posts/index.js
@@ -12,6 +12,11 @@ app.get('/posts', (req, res) => {
 
 app.post('/posts', (req, res) => {
   const id = randomBytes(4).toString('hex')
+  const { title } = req.body
+  posts[id] = {
+    id, title
+  }
+  res.status(201).send(posts[id])
 })
 
 app.listen(4000, () => {
