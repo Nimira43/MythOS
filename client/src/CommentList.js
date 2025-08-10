add48acf25eb49c639c9f20e2c98bcb62430af0c
Nimira43 <n.rabone72@aol.com>
2025-08-10 23:32:52 +0100

    fetchData method

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,14 +2,11 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export default function CommentList({ postId}) {
-   const [content, setContent] = useState('')
-  const onSubmit = async (event) => {
-    event.preventDefault()
-
-    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
-      content
-    })
-    setContent('')
+  const [comments, setComments] = useState([])
+  
+  const fetchData = async () => {
+    const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
+    setComments(res.data)
   }
   return (
     <div>
