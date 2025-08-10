ae3e016478743c22b1390d996f40f4a0bdcc4b08
Nimira43 <n.rabone72@aol.com>
2025-08-10 23:26:38 +0100

    Copied over the content from CommentCreate to CommentList

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,2 +1,29 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+
+export default function CommentList({ postId}) {
+   const [content, setContent] = useState('')
+  const onSubmit = async (event) => {
+    event.preventDefault()
+
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content
+    })
+    setContent('')
+  }
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label>New Comment</label>
+          <input 
+            className='form-control' 
+            value={content}
+            onChange={e => setContent(e.target.value)}
+          />
+        </div>
+        <button className='btn bg-main text-light'>Submit</button>
+      </form>
+    </div>
+  )
+}
