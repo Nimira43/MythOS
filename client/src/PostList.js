9d9205d43b3121588282420089b225e8b17dc572
Nimira43 <n.rabone72@aol.com>
2025-08-10 22:47:00 +0100

    Updated fetchPosts

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -6,8 +6,8 @@ export default function PostList() {
   
   const fetchPosts = async () => {
     const res = await axios.get('http://localhost:4000/posts')
+    setPosts(res.data)
   }
-
 
   return (
     <div>
