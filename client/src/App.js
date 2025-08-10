8c3c5f74f5c5d39dd7597ab8da6be94e4da31bed
Nimira43 <n.rabone72@aol.com>
2025-08-10 22:39:46 +0100

    Imported PostList component into App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import PostCreate from './PostCreate'
+import PostList from './PostList'
 
 export default function App() {
   return (
     <div className='container'>
       <h4 className='logo text-main mt-3'>MythOS</h4>
       <PostCreate />
+      <hr />
+      <h4 className='logo text-main'>Posts</h4>
+      <PostList />
     </div>
   )
 }
