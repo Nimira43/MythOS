050ba82244f0888b5d1c82f7927263c38435c401
Nimira43 <n.rabone72@aol.com>
2025-08-10 22:45:51 +0100

    Created new fetchPosts async function

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -2,8 +2,13 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export default function PostList() {
-  const [posts, setPosts] = useState()
+  const [posts, setPosts] = useState({})
   
+  const fetchPosts = async () => {
+    const res = await axios.get('http://localhost:4000/posts')
+  }
+
+
   return (
     <div>
 
