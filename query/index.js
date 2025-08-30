3d8f20dc7541526ca0d067a2f399a916a70d3f84
Nimira43 <n.rabone72@aol.com>
2025-08-30 04:37:02 +0100

    Setting up get and post request outlines.

--- query/index.js
+++ query/index.js
@@ -5,3 +5,12 @@ const cors = require('cors')
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
+
+app.get('/posts', (req, res) => {
+
+})
+
+app.post('/events', (req, res) => {
+
+})
+
