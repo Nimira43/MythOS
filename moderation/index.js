8f098246c85e956e1fa78f27a749f0671facd02b
Nimira43 <n.rabone72@aol.com>
2025-09-25 17:48:09 +0100

    Setting up empty route handler

--- moderation/index.js
+++ moderation/index.js
@@ -3,4 +3,8 @@ const bodyParser = require('body-parser')
 const axios = require('axios')
 
 const app = express()
-app.use(bodyParser.json)
+app.use(bodyParser.json())
+
+app.post('/events', (re, res) => {
+  
+})
