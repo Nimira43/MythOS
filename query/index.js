873918ce88f1802e333e91725568546b57ec5d95
Nimira43 <n.rabone72@aol.com>
2025-11-12 02:19:28 +0000

    Require axios into query index.js

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -44,7 +45,6 @@ app.get('/posts', (req, res) => {
 
 app.post('/events', (req, res) => {
   const { type, data } = req.body
-
   handleEvent(type, data)
   res.send({})
 })
