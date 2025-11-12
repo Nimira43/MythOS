599ada097c2f46626cfc38bfddb483f2ccd360e2
Nimira43 <n.rabone72@aol.com>
2025-11-12 02:07:43 +0000

    Added another endpoint that gets all the stored events in event-bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -28,6 +28,10 @@ app.post('/events', (req, res) => {
   res.send({ status: 'OK' })
 })
 
+app.get('/events', (req, res) => {
+  res.send(events)
+})
+
 app.listen(4005, () => {
   console.log('Event Bus Server - listening on Port 4005.')
 })
